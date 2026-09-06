ea03751493e805f13c1d891d6aee6c0d1f7a0636
Prueba <andr.marin@duocuc.cl>
2026-09-06 15:59:55 -0300

    validaciones para registro de mascota y dueno + cambios en el boton de los formulario + cambio de los nombres de los archivos html de las barras de navegacion por Andres Marin

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,6 @@
 const formularioProducto = document.querySelector("#formulario-nuevo-producto");
+const patronCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const patronRut = /^(\d{1,2}(?:\.\d{3}){2}|\d{7,8})-[0-9Kk]$/;
 
 if (formularioProducto) {
   formularioProducto.addEventListener("submit", function (e) {
@@ -64,4 +66,104 @@ if (formularioProducto) {
       mensajeConfirmacion.textContent = "";
     }
   });
+
+// ==========================================================================================================================================
+//====================== Formulario registro de usuario =====================================================================================
+
+const formularioRegistroMascota = document.querySelector('#formulario-mascota')
+const formularioRegistroDueno = document.querySelector('#formulario-dueno')
+
+formularioRegistroDueno.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  //variables registro mascota
+  const nombreMascota = document.querySelector('#nombre-mascota');
+  const especie = document.querySelector('#especie');
+  const raza = document.querySelector('#raza');
+  const sexoMascota = document.querySelector('#sexo');
+  const fechaNacimientoMascota = document.querySelector('#fecha-nacimiento');
+  const esterilizada = document.querySelector('input[name="esterilizada"]:checked').value;
+
+
+  //Variables registro dueno
+
+  const nombreDueno = document.querySelector('#nombre-completo');
+  const rut = document.querySelector('#rut');
+  const telefono = document.querySelector('#telefono')
+  const correo = document.querySelector('#correo');
+  const direccion = document.querySelector('#direccion');
+  const contra = document.querySelector('#contrasena');
+
+  let formularioRegistro = true;
+
+  //Validaciones registro-mascota
+
+  if (nombreMascota.value.trim() === '') {
+    nombreMascota.classList.add('campo-error');
+    formularioRegistro = false;
+  } else {
+    nombreMascota.classList.remove('campo-error');
+  }
+
+  if (especie.value.trim() === '') {
+    especie.classList.add('campo-error');
+    formularioRegistro = false;
+  } else {
+    especie.classList.remove('campo-error');
+  }
+
+  if (sexoMascota.value.trim() === '') {
+    sexoMascota.classList.add('campo-error');
+    formularioRegistro = false;
+  } else {
+    sexoMascota.classList.remove('campo-error');
+  }
+
+  //validaciones registro-dueno
+  
+  if (nombreDueno.value.trim() === '') {
+    nombreDueno.classList.add('campo-error');
+    formularioRegistro = false;
+  } else {
+    nombreDueno.classList.remove('campo-error');
+  }
+
+  if(!patronRut.test(rut.value.trim())) {
+    rut.classList.add('campo-error')
+    formularioRegistro = false;
+  } else {
+    rut.classList.remove('campo-error')
+  }
+
+  if (telefono.value.trim() === '') {
+    telefono.classList.add('campo-error');
+    formularioRegistro = false;
+  } else {
+    telefono.classList.remove('campo-error');
+  }
+
+  if(!patronCorreo.test(correo.value.trim())) {
+    correo.classList.add('campo-error');
+    formularioRegistro = false;
+  } else {
+    correo.classList.remove('campo-error');
+  }
+
+  if(direccion.value.trim() === '') {
+    direccion.classList.add('campo-error');
+    formularioRegistro = false;
+  } else {
+    direccion.classList.remove('campo-error')
+  }
+  if(contra.value.trim() === '') {
+    contra.classList.add('campo-error');
+    formularioRegistro = false;
+  } else {
+    contra.classList.remove('campo-error');
+  }
+
+  if(formularioRegistro) {
+    document.querySelector('#mensaje-confirmacion').textContent = 'Registro completado exitosamente.'
+  }
+}) 
 }
